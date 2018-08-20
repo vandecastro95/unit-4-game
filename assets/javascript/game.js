c044503b3a94a32aa442c47a418c0776442ab5b6
Laptop <vandecastro08@gmail.com>
2018-08-20 14:18:35 -0500

    working on java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,59 @@
-
 let characters = {
-    'deer' : {
-        name: 'deer',
+    'cersei': {
+        name: 'cersei',
+        health: 120,
+        attack: 8,
+        imageUrl: "../images/cersei.jpg",
+        enemyAttackBack: 15
+    }, 
+    'danaerys': {
+        name: 'danaerys',
         health: 100,
-        attack: 25,
-        counter: 25
-    },
-
-'panda' : {
-        name: 'panda',
-        health: 250,
-        attack: 30,
-        counter: 30
-    },
-
-'donkey' : {
-        name: 'donkey',
-        health: 200,
-        attack: 5,
-        counter: 10
-    },
-
-'eagle' : {
-        name: 'eagle',
+        attack: 14,
+        imageUrl: "../images/danaerys.jpg",
+        enemyAttackBack: 5
+    }, 
+    'nightking': {
+        name: 'nightking',
         health: 150,
-        attack: 30,
-        counter: 10
+        attack: 8,
+        imageUrl: "../images/nightking.jpg",
+        enemyAttackBack: 20
+    }, 
+    'jonsnow': {
+        name: 'jonsnow',
+        health: 180,
+        attack: 7,
+        imageUrl: "../images/jonsnow.jpg",
+        enemyAttackBack: 20
     }
-}
+};
+
+let attacker;
+let defender;
+
+$(document).ready(function() {
+
+    
+    $('#middle').hide();
+    $('#enemySelector').hide();
+
+     $('.char').on("click", function() {
+
+        if (attacker == null){
+        attacker = $(this).text();
+        alert(attacker);
+        $('#topSelector').hide();
+        $('#middle').show();
+        $('.attacker').css("backgroundImage", "url('attacker.imageUrl') no-repeat");
+        $('#enemySelector').show();
+        }
+
+        if (attacker != ''){
+        defender = $(this).text();
+
+        }
+
+     });
+});
+        
