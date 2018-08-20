29e17a20acfa699abdf23c176045ee7d262af943
Laptop <vandecastro08@gmail.com>
2018-08-20 11:48:15 -0500

    commiting

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+let characters = {
+    'deer' : {
+        name: 'deer',
+        health: 100,
+        attack: 25,
+        counter: 25
+    },
+
+'panda' : {
+        name: 'panda',
+        health: 250,
+        attack: 30,
+        counter: 30
+    },
+
+'donkey' : {
+        name: 'donkey',
+        health: 200,
+        attack: 5,
+        counter: 10
+    },
+
+'eagle' : {
+        name: 'eagle',
+        health: 150,
+        attack: 30,
+        counter: 10
+    }
+}
