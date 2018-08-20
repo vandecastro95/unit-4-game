f64405f93d4b6c89c088a38f7e418746b1cccb2f
unknown <vandecastro95@gmail.com>
2018-08-20 11:41:26 -0500

    added images

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+let characters = {
+    'Queen Cersei': {
+        name: 'rey',
+        health: 120,
+        attack: 8,
+        imageUrl: "assets/images/cersei.jpg",
+        enemyAttackBack: 15
+    }, 
+    'Danaerys': {
+        name: 'darth',
+        health: 100,
+        attack: 14,
+        imageUrl: "assets/images/danaerys.jpg",
+        enemyAttackBack: 5
+    }, 
+    'Jon Snow': {
+        name: 'finn',
+        health: 150,
+        attack: 8,
+        imageUrl: "assets/images/jonsnow.jpg",
+        enemyAttackBack: 20
+    }, 
+    'Ice King': {
+        name: 'stormtrooper',
+        health: 180,
+        attack: 7,
+        imageUrl: "assets/images/nightking.png",
+        enemyAttackBack: 20
+    }
+};
